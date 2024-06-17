dc7fc49c242644cca7d973b9413f2d42867334ae
HampCode <pcsiheme@upc.edu.pe>
2024-06-16 20:30:09 -0500

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment={
     producction: false,
-    apiUrl: 'http://localhost:3000/api/v1'
+    //apiUrl: 'http://localhost:3000/api/v1'
+    //apiUrl: 'http://localhost:8080/api/v1'
+    apiUrl: 'https://banking-api-g0w2.onrender.com/api/v1'
 }
