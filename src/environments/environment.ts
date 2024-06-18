2166e08f87de2b3db67c8d094963eedbab825ce5
HampCode <pcsiheme@upc.edu.pe>
2024-06-17 20:03:23 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment={
     producction: false,
     //apiUrl: 'http://localhost:3000/api/v1'
     //apiUrl: 'http://localhost:8080/api/v1'
-    apiUrl: 'https://banking-api-fake.onrender.com/api/v1'
+    //apiUrl: 'https://banking-api-fake.onrender.com/api/v1'
+    apiUrl: 'https://banking-api-fake-1.onrender.com/api/v1'
 }
